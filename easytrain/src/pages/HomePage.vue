04fdfc05132d3777d3091ff3296ba750375db2b6
Vinmania <vinmaniamc@gmail.com>
2021-10-11 14:24:42 +0200

    initial Vue.js

---
+++ easytrain/src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Bienvenue sur Easy Train</h2>
+        <div>qmqm</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "homepage",
+    props: {},
+    components: {},
+};
+</script>
+
+
+<style scoped>
+</style>
